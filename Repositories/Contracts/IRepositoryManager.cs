72ab52cf042ad1488cd2a358e0d9ca25672a6e54
Emircan Furkan Bayendur <26100043@arcelik.com>
2025-02-25 16:19:59 +0300

    lazy loading and repo manager

---
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    // Bu manager classinda unit of work patterni uygulanmistir. bu pattern, bir veya birden fazla repository sınıfını bir arada kullanmamızı sağlar.
+    // Bu sayede bir repository sınıfı üzerinde işlem yaparken, diğer repository sınıflarını da kullanabiliriz.
+    // RepositoryManager butun repository sınıflarını tek bir yerde toplar.
+    public interface IRepositoryManager
+    {
+
+        IBookRepository Book { get; }
+
+        // save metodu, butun repository sınıflarındaki degisiklikleri veritabanına kaydeder.
+        // repolarda db context ile degisiklik yaptiktan sonra manager uzerindeki save metodu cagirilarak degisiklikler kaydedilir.
+        // repositoryler sadece degisiklik yapar, degisiklikleri kaydetme islemi manager uzerinde yapilir.
+        void Save();
+    }
+}
---
+++ Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,44 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    // Bu manager classinda unit of work patterni uygulanmistir. bu pattern, bir veya birden fazla repository sınıfını bir arada kullanmamızı sağlar.
+    // Bu sayede bir repository sınıfı üzerinde işlem yaparken, diğer repository sınıflarını da kullanabiliriz.
+    // RepositoryManager butun repository sınıflarını tek bir yerde toplar.
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+
+        // LAZY loading yapiyoruz. sadece cagrildiginda new'lenecek.
+        private readonly Lazy<IBookRepository> _bookRepository;
+
+        // save islemini context uzerinden yapacagimiz icin contexti burada inject ediyoruz.
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+
+            // burada new'lemek yerine repository sınıflarını inject edebiliriz.
+            _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_context));
+
+        }
+
+
+        
+        public IBookRepository Book => _bookRepository.Value;
+
+
+
+        // save metodu, butun repository sınıflarındaki degisiklikleri veritabanına kaydeder.
+        // repolarda db context ile degisiklik yaptiktan sonra manager uzerindeki save metodu cagirilarak degisiklikler kaydedilir.
+        // repositoryler sadece degisiklik yapar, degisiklikleri kaydetme islemi manager uzerinde yapilir.
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
